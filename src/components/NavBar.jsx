576ec496b74dbe75fa6d34c291128e98ea2d1c96
Don Franklie <donfranklie@gmail.com>
2025-04-29 17:09:38 +0300

    Refactor NavBar component: improve layout and update styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { HiExternalLink } from "react-icons/hi";
 
 const NavBar = () => {
   return (
     <>
-      <div className="w-full lg:h-[80px] md:h-[80px] lg:container lg:mx-auto lg:px-32 md:px-8 md:mx-2 sm:px-2 sm:mx-2 sm:h-fit">
-        <div className="flex items-center justify-start h-full px-8 pt-4">
+      <div className="w-full lg:h-[80px] md:h-[80px] lg:container lg:mx-auto lg:px-32 md:px-8 md:mx-2 sm:px-2 sm:mx-2 sm:h-fit border-b-[.5px] border-gray-500">
+        <div className="flex items-center justify-between h-full px-8 pt-4">
           <a
             href="https://franklinmakokha.vercel.app/"
             target="_blank"
-            className="sm:text-xl lg:text-2xl md:text-2xl text-2xl text-gray-600 cursor-pointer"
+            className="sm:text-xl lg:text-xl md:text-xl text-xl text-gray-400 cursor-pointer hero-text"
           >
             DevFranklin
           </a>
@@ -16,6 +18,16 @@ const NavBar = () => {
             <span className="hover:text-white cursor-pointer">Projects</span>
             <span className="hover:text-white cursor-pointer">Resume</span>
           </div> */}
+          <div>
+            <a
+              href="https://github.com/DonFranklie"
+              target="_blank"
+              className="text-xl inline-flex gap-1"
+            >
+              <FaGithub className="text-white" />{" "}
+              <HiExternalLink className="justify-self-start text-xs" />
+            </a>
+          </div>
         </div>
       </div>
     </>
