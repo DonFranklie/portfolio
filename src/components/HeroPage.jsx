842859613f602966738ee26c005c53ef5e79c56b
Don Franklie <donfranklie@gmail.com>
2025-04-29 17:09:34 +0300

    Refactor HeroPage component: update text and remove unused button

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -7,27 +7,13 @@ const HeroPage = () => {
     <>
       <div className="container mx-auto lg:px-64 h-[300px] lg:text-6xl text-2xl md:text-6xl md:px-20 text-center pt-8 flex items-center justify-center">
         <div className="flex flex-col items-center justify-center">
-          <div className="relative mb-4">
-            <div className="w-[144px] h-[33px] absolute bg-gradient-to-r from-gray-700 to-gray-300 rounded-full inset-0"></div>
-            <button className="text-xl top-[1px] left-[1px] relative w-[calc(142px-1px)] h-[calc(32px-1px)] bg-black px-2  rounded-full flex flex-row items-center justify-between gap-2">
-              <a
-                href="https://github.com/DonFranklie"
-                target="_blank"
-                className="text-sm"
-              >
-                My Github
-              </a>{" "}
-              <span>
-                <FaArrowCircleRight className="text-base" />
-              </span>
-            </button>
-          </div>
-          <div className="bg-gradient-to-r from-gray-600 via-white to-gray-600 bg-clip-text text-transparent hover:bg-gradient-to-r hover:from-teal-400 hover:via-blue-500 hover:to-purple-600 transition-all duration-300 lg:text-6xl md:text-6xl sm:text-5xl text-4xl">
-            <p>Hello! I'm Franklin,</p>
-            <p>a Full-Stack Developer</p>
+          <div className="bg-gradient-to-r from-gray-600 via-white to-gray-600 bg-clip-text text-transparent transition-all duration-300 lg:text-6xl md:text-6xl sm:text-5xl text-4xl hero-text">
+            <span className="text-emerald-400 text-base">Hello! I'm </span>
+            <p> Franklin Makokha,</p>
+            <p>a Software Developer</p>
           </div>
           <p className="text-sm mt-3 text-gray-400">
-            I help you transform ideas into scalable, impactful web solutions
+            I transform ideas into scalable, impactful web solutions
           </p>
         </div>
       </div>
