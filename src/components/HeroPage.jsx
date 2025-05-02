bf413daeb11080d36a27ce3157ed3ccc2e1592e1
Don Franklie <donfranklie@gmail.com>
2025-05-02 10:49:00 +0300

    Refactor HeroPage component: adjust layout and centering for improved responsiveness

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -5,8 +5,8 @@ import { FaArrowRightArrowLeft } from "react-icons/fa6";
 const HeroPage = () => {
   return (
     <>
-      <div className="container mx-auto lg:px-64 h-[300px] lg:text-6xl text-2xl md:text-6xl md:px-20 text-center pt-8 flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
+      <div className="container mx-auto lg:px-64 h-[300px] lg:text-6xl text-2xl md:text-6xl md:px-20 pt-8 px-4 flex">
+        <div className="flex flex-col justify-center">
           <div className="bg-gradient-to-r from-gray-600 via-white to-gray-600 bg-clip-text text-transparent transition-all duration-300 lg:text-6xl md:text-6xl sm:text-5xl text-4xl hero-text">
             <span className="text-emerald-400 text-base">Hello! I'm </span>
             <p> Franklin Makokha,</p>
