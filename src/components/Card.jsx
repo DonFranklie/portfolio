8a2730a608bdcb6a6569ad7c13c323c46a179387
Don Franklie <donfranklie@gmail.com>
2025-05-02 16:56:48 +0300

    Update Card component: adjust button styling and replace icon for consistency

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { FiArrowRightCircle } from "react-icons/fi";
+import { MdOutlineArrowOutward } from "react-icons/md";
 
 const Card = ({ name, description, technologiesUsed }) => {
   return (
-    <div className="w-[300px] h-[200px] border-[.5px] border-emerald-300 rounded-md p-4 flex flex-col gap-10 text-white m-2">
+    <div className="w-[300px] h-[200px] border-[.5px] border-emerald-300 rounded-md p-4 flex flex-col gap-12 text-white m-2">
       <div className="flex flex-col gap-2">
         <h4 className="text-base hero-text">{name}</h4>
         <p className="text-gray-600 text-xs">{description}</p>
@@ -11,8 +12,8 @@ const Card = ({ name, description, technologiesUsed }) => {
       </div>
 
       <div>
-        <button className="px-4 py-2 flex items-center gap-2 text-sm border-[.5px] rounded-sm border-emerald-700">
-          Read More <FiArrowRightCircle className="text-base" />
+        <button className="px-4 py-1 flex items-center gap-2 text-sm border-[.5px] rounded-sm border-emerald-700">
+          Read More <MdOutlineArrowOutward className="text-base" />
         </button>
         {/* <button>Read More</button> */}
       </div>
