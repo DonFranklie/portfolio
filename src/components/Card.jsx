796fc7698851a7aaef22f9cab57a9dd9f4f4c683
Don Franklie <donfranklie@gmail.com>
2025-05-02 16:53:02 +0300

    Refactor Card component: rename props for clarity and consistency

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FiArrowRightCircle } from "react-icons/fi";
 
-const Card = ({ name, desc, tech }) => {
+const Card = ({ name, description, technologiesUsed }) => {
   return (
     <div className="w-[300px] h-[200px] border-[.5px] border-emerald-300 rounded-md p-4 flex flex-col gap-10 text-white m-2">
       <div className="flex flex-col gap-2">
         <h4 className="text-base hero-text">{name}</h4>
-        <p className="text-gray-600 text-xs">{desc}</p>
-        <div>{tech}</div>
+        <p className="text-gray-600 text-xs">{description}</p>
+        <div>{technologiesUsed}</div>
       </div>
 
       <div>
