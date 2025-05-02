bee11b881cf44c80dd06ef408b8262a97b26d10b
Don Franklie <donfranklie@gmail.com>
2025-05-02 10:48:47 +0300

    Refactor App component: add Projects component to the main layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import HeroPage from "./components/HeroPage";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+
+import { FaCss3Alt, FaHtml5, FaJs, FaPhp, FaBootstrap } from "react-icons/fa6";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiMysql, SiPhp } from "react-icons/si";
 
 function App() {
   return (
-    <div className="min-h-screen relative bg-black text-white border-box">
+    <div className="min-h-screen bg-black text-white border-box">
       <NavBar />
       <HeroPage />
       <Skills />
+      <Projects />
       <Footer />
     </div>
   );
