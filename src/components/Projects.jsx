a7852cd9f3a7d4a8bb700d25118fe65070c43e3b
Don Franklie <donfranklie@gmail.com>
2025-05-02 11:09:17 +0300

    Fix import case for Card component in Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./card";
+import Card from "./Card";
 import { FaCss3Alt, FaHtml5, FaJs, FaPhp, FaBootstrap } from "react-icons/fa6";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiMysql, SiPhp } from "react-icons/si";
