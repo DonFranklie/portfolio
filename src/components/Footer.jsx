a70eea0d2f5b7d11828ab447db10caa152946472
Don Franklie <donfranklie@gmail.com>
2025-05-02 10:48:56 +0300

    Refactor Footer component: remove unnecessary absolute positioning from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-5 py-8 flex items-center justify-center gap-2 absolute bottom-0">
+    <footer className="w-full h-5 py-8 flex items-center justify-center gap-2">
       <span>Email me at </span>
       <a href="mailto:makokhafranklyn@gmail.com" className="text-blue-600">
         makokhafranklyn@gmail.com
